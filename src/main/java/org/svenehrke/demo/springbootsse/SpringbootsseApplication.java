5e7cffe3539cdb5989b93b1922191049bc181be5
Sven Ehrke <sven.ehrke@sven-ehrke.de>
2025-01-05 19:37:24 +0100

    initial files

---
+++ src/main/java/org/svenehrke/demo/springbootsse/SpringbootsseApplication.java
@@ -0,0 +1,13 @@
+package org.svenehrke.demo.springbootsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/svenehrke/demo/springbootsse/SpringbootsseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.svenehrke.demo.springbootsse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
